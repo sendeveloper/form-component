cb0cdf79b1ac58b64067a6c6a05cc589203373f9
sendeveloper <lightmorning46@gmail.com>
2017-12-01 00:43:57 +0200

    updating

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import FormContainer from './subcomponent/FormContainer';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <div className="columns">
+          <div className="col-md-9 centered">
+            <h3>React.js Controlled Form Components</h3>
+            <FormContainer />
+          </div>
+        </div>
       </div>
     );
   }
